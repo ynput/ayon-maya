44b49c97cc5c459e8068bc0058fd92ad57fee396
Kayla Man <moonyuet2008@hotmail.com>
2024-07-03 21:18:38 +0800

    fix the bug on ornatrix loader

--- client/ayon_maya/plugins/load/load_ornatrix_alembic.py
+++ client/ayon_maya/plugins/load/load_ornatrix_alembic.py
@@ -1,12 +1,15 @@
 from maya import cmds
+
+from ayon_core.settings import get_project_settings
 from ayon_core.pipeline import get_representation_path
 from ayon_maya.api import plugin
-from ayon_maya.api.lib import unique_namespace, maintained_selection
+from ayon_maya.api.pipeline import containerise
+from ayon_maya.api.lib import maintained_selection, unique_namespace
+from ayon_maya.api.plugin import get_load_color_for_product_type
 from ayon_core.lib import EnumDef
 
 
-
-class OxAlembicLoader(plugin.ReferenceLoader):
+class OxAlembicLoader(plugin.Loader):
     """Ornatrix Alembic Loader"""
 
     product_types = {"oxcache"}
@@ -19,60 +22,70 @@ class OxAlembicLoader(plugin.ReferenceLoader):
 
     @classmethod
     def get_options(cls, contexts):
-        return cls.options + [
+        return [
             EnumDef(
                 "import_options",
-                items={
-                    0: "Hair",
-                    1: "Guides"
-                },
-                default=0
-            )
-        ]
-
-    def process_reference(self, context, name, namespace, options):
-        cmds.loadPlugin("Ornatrix", quiet=True)
-        folder_name = context["folder"]["name"]
-        namespace = namespace or unique_namespace(
-            folder_name + "_",
-            prefix="_" if folder_name[0].isdigit() else "",
-            suffix="_",
+            label="Import Options for Ornatrix Cache",
+            items={
+                0: "Hair",
+                1: "Guide"
+            },
+            default=0,
         )
+    ]
 
-        attach_to_root = options.get("attach_to_root", True)
-        group_name = options["group_name"]
-
-        # no group shall be created
-        if not attach_to_root:
-            group_name = namespace
+    def load(self, context, name, namespace, options):
+        cmds.loadPlugin("Ornatrix", quiet=True)
+        product_type = context["product"]["productType"]
+        # Build namespace
+        folder_name = context["folder"]["name"]
+        if namespace is None:
+            namespace = self.create_namespace(folder_name)
 
         path = self.filepath_from_context(context)
+        path = path.replace("\\", "/")
+
         ox_import_options = "; importAs={}".format(
             options.get("import_options"))
+        group_name = "{}:{}".format(namespace, name)
+        project_name = context["project"]["name"]
 
+        new_nodes = []
         with maintained_selection():
-            file_url = self.prepare_root_value(
-                path, context["project"]["name"]
-            )
             nodes = cmds.file(
-                file_url,
+                path,
+                i=True,
                 type="Ornatrix Alembic Import",
                 namespace=namespace,
+                returnNewNodes=True,
                 groupName=group_name,
                 options=ox_import_options
             )
+            nodes = cmds.ls(nodes)
+            shapes = cmds.ls(nodes, shapes=True)
+            ox_node = cmds.ls(nodes, type="BakedHairNode")
+            new_nodes = (list(set(nodes) - set(shapes)- set(ox_node)))
+        group_node = cmds.group(new_nodes, name=group_name)
 
-        color = plugin.get_load_color_for_product_type("oxcache")
+        settings = get_project_settings(project_name)
+        color = get_load_color_for_product_type(product_type, settings)
         if color is not None:
             red, green, blue = color
-            cmds.setAttr(group_name + ".useOutlinerColor", 1)
-            cmds.setAttr(
-                group_name + ".outlinerColor", red, green, blue
-            )
+            cmds.setAttr(group_node + ".useOutlinerColor", 1)
+            cmds.setAttr(group_node + ".outlinerColor", red, green, blue)
 
-        self[:] = nodes
+        new_nodes.append(group_node)
 
-        return nodes
+        self[:] = new_nodes
+
+        return containerise(
+            name=name,
+            namespace=namespace,
+            nodes=new_nodes,
+            context=context,
+            loader=self.__class__.__name__
+        )
+
 
     def update(self, container, context):
         repre_entity = context["representation"]
@@ -84,3 +97,37 @@ class OxAlembicLoader(plugin.ReferenceLoader):
 
     def switch(self, container, context):
         self.update(container, context)
+
+    def remove(self, container):
+
+        namespace = container["namespace"]
+        nodes = container["nodes"]
+
+        self.log.info("Removing '%s' from Maya.." % container["name"])
+
+        nodes = cmds.ls(nodes, long=True)
+
+        try:
+            cmds.delete(nodes)
+        except ValueError:
+            # Already implicitly deleted by Maya upon removing reference
+            pass
+
+        cmds.namespace(removeNamespace=namespace, deleteNamespaceContent=True)
+
+    def create_namespace(self, folder_name):
+        """Create a unique namespace
+        Args:
+            asset (dict): asset information
+
+        """
+
+        asset_name = "{}_".format(folder_name)
+        prefix = "_" if asset_name[0].isdigit() else ""
+        namespace = unique_namespace(
+            asset_name,
+            prefix=prefix,
+            suffix="_"
+        )
+
+        return namespace
