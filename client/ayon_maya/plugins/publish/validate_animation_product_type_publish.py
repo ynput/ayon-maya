00c937f36bf5099db80d56b4566b48d184c72fef
Kayla Man <moonyuet2008@hotmail.com>
2024-08-15 11:46:32 +0800

    add validator back to check on the animation product type

---
+++ client/ayon_maya/plugins/publish/validate_animation_product_type_publish.py
@@ -0,0 +1,42 @@
+import ayon_maya.api.action
+from ayon_core.pipeline.publish import (
+    PublishValidationError,
+    ValidateContentsOrder,
+)
+from ayon_maya.api import plugin
+
+
+class ValidateAnimationProductTypePublish(plugin.MayaInstancePlugin):
+    """Validate Animation Product Type to ensure either fbx animation
+    collector or collect animation output geometry(Alembic) enabled for
+    publishing
+    """
+
+    order = ValidateContentsOrder
+    families = ["animation"]
+    label = "Animation Product Type Publish"
+    actions = [ayon_maya.api.action.SelectInvalidAction]
+
+    @classmethod
+    def get_invalid(cls, instance):
+        invalid = []
+        publish_attributes = instance.data["publish_attributes"]
+        if "animation.fbx" in instance.data["families"]:
+            return invalid
+        elif publish_attributes["ExtractAnimation"]["active"]:
+            return invalid
+        else:
+            invalid.append(instance.name)
+
+        return invalid
+
+    def process(self, instance):
+        invalid = self.get_invalid(instance)
+        if invalid:
+            message = (
+                f"Invalid Animation Product Type in {invalid}\n"
+                "Users must turn on either 'Collect Fbx Animation'\n"
+                "or 'Collect Animation Output Geometry(Alembic)'\n"
+                "for publishing\n"
+            )
+            raise PublishValidationError(message)
