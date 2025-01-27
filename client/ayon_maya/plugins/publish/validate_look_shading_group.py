cdd678264f69317d5b4cffe4b12faa463d6992c5
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-28 00:36:02 +0100

    - Add description to validation report for Validate Shading Engine Names validator - Optimize query and avoid reporting duplicates - Sort the logs for readability - Report the nodes in a bullet point list for better readability - Ignore referenced and default shading engines because they can't be renamed anyway and should be captured by other validators instead

--- client/ayon_maya/plugins/publish/validate_look_shading_group.py
+++ client/ayon_maya/plugins/publish/validate_look_shading_group.py
@@ -1,3 +1,5 @@
+import inspect
+
 import ayon_maya.api.action
 from ayon_core.pipeline.publish import (
     OptionalPyblishPluginMixin,
@@ -31,34 +33,53 @@ class ValidateShadingEngine(plugin.MayaInstancePlugin,
 
         invalid = self.get_invalid(instance)
         if invalid:
+            node_list = "\n".join(f"- {node}" for node in invalid)
             raise PublishValidationError(
-                "Found shading engines with incorrect naming:"
-                "\n{}".format(invalid)
+                "Found assigned shaders with incorrect names:"
+                "\n{}".format(node_list),
+                description=self.get_description()
             )
 
     @classmethod
     def get_invalid(cls, instance):
         shapes = cmds.ls(instance, type=["nurbsSurface", "mesh"], long=True)
+        if not shapes:
+            return []
+
+        ignored_default_nodes = set(cmds.ls(defaultNodes=True))
+        shading_engines = set(cmds.listConnections(
+            shapes, destination=True, type="shadingEngine"
+        ) or [])
         invalid = []
-        for shape in shapes:
-            shading_engines = cmds.listConnections(
-                shape, destination=True, type="shadingEngine"
-            ) or []
-            for shading_engine in shading_engines:
-                materials = cmds.listConnections(
-                    shading_engine + ".surfaceShader",
-                    source=True, destination=False
-                )
-                if not materials:
+        for shading_engine in sorted(shading_engines):
+            materials = cmds.listConnections(
+                shading_engine + ".surfaceShader",
+                source=True, destination=False
+            )
+            if not materials:
+                cls.log.warning(
+                    "Shading engine '{}' has no material connected to its "
+                    ".surfaceShader attribute.".format(shading_engine))
+                continue
+
+            material = materials[0]  # there should only ever be one input
+            name = material + "SG"
+            if shading_engine != name:
+                # Ignore referenced or read-only shading engines
+
+                if cmds.referenceQuery(shading_engine,
+                                       isNodeReferenced=True):
                     cls.log.warning(
-                        "Shading engine '{}' has no material connected to its "
-                        ".surfaceShader attribute.".format(shading_engine))
+                        "Ignoring referenced shading engine "
+                        f"with invalid name: {shading_engine}")
+
+                if shading_engine in ignored_default_nodes:
+                    cls.log.warning(
+                        "Ignoring default shading engine "
+                        f"with invalid name: {shading_engine}")
                     continue
 
-                material = materials[0]  # there should only ever be one input
-                name = material + "SG"
-                if shading_engine != name:
-                    invalid.append(shading_engine)
+                invalid.append(shading_engine)
 
         return list(set(invalid))
 
@@ -71,3 +92,16 @@ class ValidateShadingEngine(plugin.MayaInstancePlugin,
                 + "SG"
             )
             cmds.rename(shading_engine, name)
+
+    @staticmethod
+    def get_description():
+        return inspect.cleandoc("""
+            ### Shaders found with incorrect names
+            
+            The shading engine of a shader should be named after the connected
+            material to its `.surfaceShader` attribute. The name should be
+            `"{material}SG"`.
+            
+            Use the repair action to rename the shading engine to the correct
+            names automatically.
+        """)
