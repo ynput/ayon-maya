87a39f7bfac57010d0058073898a099dae138890
pavithraj <pavithraj@phantomfx.com>
2025-05-06 09:04:57 +0530

    updated anim export and imp

---
+++ client/ayon_maya/plugins/load/load_anim.py
@@ -0,0 +1,147 @@
+import os
+import json
+import maya.cmds as cmds
+import ayon_api
+from ayon_core.pipeline import get_representation_path
+from ayon_core.settings import get_project_settings
+from ayon_maya.api.lib import unique_namespace
+from ayon_maya.api.pipeline import containerise
+from ayon_maya.api import plugin
+from ayon_maya.api.plugin import get_load_color_for_product_type
+from ayon_core.pipeline.load.plugins import discover_loader_plugins
+from ayon_core.pipeline.load.utils import get_representation_context
+# from ayon_maya.plugins.load import load_referenece
+import logging
+import pymel.core as pm
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+LOADER_PLUGINS = {
+    "reference": "ReferenceLoader"
+}
+
+
+class AnimLoader(plugin.Loader):
+    """Load anim on character"""
+
+    product_types = {"animation"}
+    representations = {"anim"}
+
+    label = "Load Anim"
+    order = -5
+    icon = "code-fork"
+    color = "orange"
+
+    def load(self, context, name, namespace, data):
+        project_name = context['project']['name']
+        anim_file = context['representation']['attrib']['path']
+        self.log.info(f"anim_file: {anim_file}")
+        plugins = {i.__name__: i for i in discover_loader_plugins(project_name)}
+        name_space = context['representation']['data']['context']['product']['name'].replace(
+            context['representation']['data']['context']['product']['type'], '')
+        if not cmds.namespace(exists=name_space):
+            assets = context['version']['data'].get("assets", [])
+            self.log.info(f"assets: {assets}")
+            current_asset = [asset for asset in assets if asset['asset_name'] in anim_file]
+            if not current_asset:
+                self.log.warning(f"Asset not found in version data.")
+                return
+            current_asset = current_asset[0]
+            asset_data = ayon_api.get_folder_by_name(project_name=project_name, folder_name=current_asset['asset_name'])
+            versions = ayon_api.get_last_version_by_product_name(project_name, "rigMain", asset_data['id'])
+            representations = ayon_api.get_representations(
+                project_name=project_name,
+                version_ids={versions['id']},
+                fields={"id", "name", "files.path"}
+            )
+            rep_id = None
+            for rep in representations:
+                if rep['name'] == 'ma':
+                    rep_id = rep['id']
+                    break
+            context = get_representation_context("PRPT", rep_id)
+            options = {'attach_to_root': True}
+            _plugin = plugins.get(LOADER_PLUGINS["reference"])()
+            _plugin.load(context=context, name=context['product']['name'], namespace=name_space, options=options)
+        ctrl_set = pm.ls(name_space + ":rigMain_controls_SET")
+        if not ctrl_set:
+            self.log.warning("No control set found in instance data")
+            return
+        ctrls = pm.listConnections(ctrl_set[0], source=1, type='transform')
+        if not ctrls:
+            self.log.warning("No controls found in instance data")
+            return
+        self.log.debug(f"ctrls: {ctrls}")
+        self.log.info(f"namespace: {namespace}")
+        read_anim(filepath=anim_file, objects=ctrls, namespace=name_space)
+
+
+def read_anim(filepath='C:/temp/anim.anim', objects=pm.selected(), namespace=None):
+    if not os.path.exists(filepath):
+        return [False, 'invalid filepath']
+    with open(filepath, "r", encoding='utf-8') as reader:
+        anim_data = json.loads(reader.read())
+    for j, obj in enumerate(objects):
+        obj_shot_name = obj.name()
+        obj_longname = obj.longName()
+        if namespace:
+            obj_longname = obj_longname.replace(namespace, '{namespace}')
+        ctrl_value = anim_data.get(obj_longname, [])
+        if not ctrl_value:
+            continue
+        for attrs in ctrl_value:
+            if not hasattr(obj, attrs):
+                continue
+            if attrs in ['lock']:
+                continue
+            try:
+                cur_attr = getattr(obj, attrs)
+            except AttributeError as e:
+                logger.warning('skipping for {0} as attribute {1} was not found'.format(obj_shot_name, attrs))
+                continue
+            key_type = ctrl_value[attrs]['type']
+            if key_type == 'static':
+                key_value = ctrl_value[attrs]['value']
+                connected = pm.listConnections(cur_attr, destination=False, source=True)
+                if not connected and not cur_attr.isLocked():
+                    cur_attr.set(key_value)
+            if key_type == 'keyed':
+                key_values = ctrl_value[attrs]['keys']
+                infinity_data = ctrl_value[attrs]['infinity']
+                pre_infinity = json.loads(infinity_data.get('preInfinity'))
+                post_infinity = json.loads(infinity_data.get('postInfinity'))
+                weighted_tangents = json.loads(infinity_data.get('weightedTangents'))
+                for keys in key_values:
+                    time = json.loads(keys.get('key'))
+                    value = json.loads(keys.get('value'))
+                    breakdown = json.loads(keys.get('breakdown'))
+                    tan_lock = json.loads(keys.get('lock'))
+                    weight_lock = json.loads(keys.get('weightLock'))
+                    in_type = json.loads(keys.get('inTangentType'))
+                    out_type = json.loads(keys.get('outTangentType'))
+                    tan1 = json.loads(keys.get('inAngle'))
+                    tan2 = json.loads(keys.get('outAngle'))
+                    weight1 = json.loads(keys.get('inWeight'))
+                    weight2 = json.loads(keys.get('outWeight'))
+                    res = pm.setKeyframe(cur_attr, time=time, value=value, bd=breakdown)
+                    if weighted_tangents:
+                        pm.keyTangent(cur_attr, weightedTangents=True, edit=True)
+                    try:
+                        pm.keyTangent(cur_attr, lock=tan_lock, time=time)
+                    except Exception as e:
+                        logger.warning(e)
+
+                    if weighted_tangents:
+                        pm.keyTangent(cur_attr, time=time, weightLock=weight_lock)
+                    if in_type != 'fixed' and out_type != 'fixed':
+                        pm.keyTangent(cur_attr, e=1, a=1, time=time, itt=in_type, ott=out_type)
+                    if in_type == 'fixed' and out_type != 'fixed':
+                        pm.keyTangent(cur_attr, e=1, a=1, time=time, inAngle=tan1, inWeight=weight1, itt=in_type,
+                                      ott=out_type)
+                    if in_type == 'fixed' and out_type == 'fixed':
+                        pm.keyTangent(cur_attr, e=1, a=1, time=time, inAngle=tan1, inWeight=weight1, outAngle=tan2,
+                                      outWeight=weight2, itt=in_type, ott=out_type)
+
+                    pm.setInfinity(cur_attr, poi=post_infinity, pri=pre_infinity)
+    return None
---
+++ client/ayon_maya/plugins/publish/collect_anim_curve.py
@@ -0,0 +1,142 @@
+import os
+import json
+import logging
+import pymel.core as pm
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+from ayon_maya.api import plugin
+from pyblish.api import ExtractorOrder
+
+
+class ExtractAnimCrv(plugin.MayaExtractorPlugin):
+    order = ExtractorOrder
+    label = "Extract Animation curves (TEST)"
+    families = ["animation"]
+    hosts = ["maya"]
+
+    def process(self, instance):
+        instance_data = instance.data
+        # Define output path
+        staging_dir = self.staging_dir(instance)
+        filename = "{0}.anim".format(instance_data['variant'])
+        out_path = os.path.join(staging_dir, filename)
+        controls = [x for x in instance_data['setMembers'] if x.endswith(":rigMain_controls_SET")]
+        if not controls:
+            self.log.warning("No controls found in instance data")
+            return
+        ctrls = pm.listConnections(controls[0], source=1, type='transform')
+        name_space = instance_data['variant']
+        reference_node = [x for x in pm.listReferences() if x.namespace == name_space][0]
+        self.log.info(f"controls: {controls}")
+        self.write_anim(objects=ctrls, filepath=os.path.realpath(out_path), namespace=name_space)
+        if "representations" not in instance.data:
+            instance.data["representations"] = []
+        representation = {
+            'name': 'anim', 'ext': 'anim',
+            'files': os.path.basename(out_path),
+            'stagingDir': staging_dir.replace("\\", "/")
+        }
+        version_data = instance.data.get("versionData", {})
+        assets = version_data.get("assets", [])
+        if not assets:
+            version_data["assets"] = []
+        asset_data = {
+            "namespace": name_space,
+            "path": reference_node.path.__str__(),
+            "asset_name": name_space.split("_rigMain_")[0],
+        }
+        version_data["assets"].append(asset_data)
+        instance_data["data"] = [name_space + ':' + reference_node.path.__str__()]
+        instance.data["assets"] = [name_space + ':' + reference_node.path.__str__()]
+        instance.data["versionData"] = version_data
+        self.log.info(f"representation: {representation}")
+        instance.data["representations"].append(representation)
+
+    def write_anim(self, objects=pm.selected(), filepath='C:/temp/anim.anim', namespace=None):
+        self.log.info(f"objects: {objects}")
+        self.log.info(f"Writing animation curves to {filepath}")
+        self.log.info(f"namespace: {namespace}")
+        anim_data = {}
+        for j, obj in enumerate(objects):
+            obj_shot_name = obj.name()
+            obj_longname = obj.longName()
+            if namespace:
+                obj_longname = obj_longname.replace(namespace, '{namespace}')
+            anim_data[obj_longname] = {}
+
+            channels = obj.listConnections(type='animCurve', connections=True, s=1, d=0)
+            channel_dict = {}
+            for i, channel in enumerate(channels):
+                channel = channel[1]
+                split_name = obj_shot_name
+                channel_name = (channels[i][0].name().split(split_name + '.')[1])
+                if channel_name not in channel_dict:
+                    channel_dict[channel_name] = {}
+                channel_dict[channel_name]['type'] = 'keyed'
+
+                keys = pm.animation.keyframe(channel, q=True)
+                values = pm.animation.keyframe(channel, q=True, valueChange=True)
+                breakdown = pm.animation.keyframe(channel, q=True, breakdown=True)
+                in_tangent_type = pm.animation.keyTangent(channel, q=True, inTangentType=True)
+                out_tangent_type = pm.animation.keyTangent(channel, q=True, outTangentType=True)
+                lock = pm.animation.keyTangent(channel, q=True, lock=True)
+                weight_lock = pm.animation.keyTangent(channel, q=True, weightLock=True)
+                in_angle = pm.animation.keyTangent(channel, q=True, inAngle=True)
+                out_angle = pm.animation.keyTangent(channel, q=True, outAngle=True)
+                in_weight = pm.animation.keyTangent(channel, q=True, inWeight=True)
+                out_weight = pm.animation.keyTangent(channel, q=True, outWeight=True)
+                weighted_tangents = pm.animation.keyTangent(channel, q=True, weightedTangents=True)[0]
+
+                pre_infinity = channel.preInfinity.get()
+                post_infinity = channel.postInfinity.get()
+                channel_dict[channel_name]['infinity'] = {
+                    'preInfinity': json.dumps(pre_infinity),
+                    'postInfinity': json.dumps(post_infinity),
+                    'weightedTangents': json.dumps(weighted_tangents),
+                }
+
+                channel_dict[channel_name]['keys'] = []
+                for y, key in enumerate(keys):
+                    bd = 0
+                    for bd_item in breakdown:
+                        if bd_item == key:
+                            bd = 1
+                    channel_dict[channel_name]['keys'].append({
+                        'key': json.dumps(keys[y]),
+                        'value': json.dumps(values[y]),
+                        'breakdown': json.dumps(bd),
+                        'inTangentType': json.dumps(in_tangent_type[y]),
+                        'outTangentType': json.dumps(out_tangent_type[y]),
+                        'lock': json.dumps(lock[y]),
+                        'weightLock': json.dumps(weight_lock[y]),
+                        'inAngle': json.dumps(in_angle[y]),
+                        'outAngle': json.dumps(out_angle[y]),
+                        'inWeight': json.dumps(in_weight[y]),
+                        'outWeight': json.dumps(out_weight[y])
+                    })
+            static_chans = pm.listAnimatable(obj)
+            for static_chan in static_chans:
+                test_it = pm.keyframe(static_chan, q=True)
+                connected = pm.listConnections(static_chan, destination=False, source=True)
+                if test_it or connected:
+                    logger.warning('skipping for {0} as attribute {1} is connected'.format(obj_shot_name, static_chan))
+                    continue
+                if pm.nodeType(static_chan.name().split(".")[0]) == "camera":
+                    static_name = static_chan.name().split('.')[1]
+                else:
+                    static_name = static_chan.name().split(obj_shot_name + '.')
+                    if not len(static_name) > 1:
+                        continue
+                    static_name = static_name[1]
+                # static_name = static_chan.name().split(obj_shot_name + '.')[1]
+                if static_name not in channel_dict:
+                    channel_dict[static_name] = {'type': 'static'}
+                channel_dict[static_name]['value'] = static_chan.get()
+            anim_data[obj_longname] = channel_dict
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath))
+        with open(filepath, 'w') as json_file:
+            json.dump(anim_data, json_file, indent=4)
+        return filepath
