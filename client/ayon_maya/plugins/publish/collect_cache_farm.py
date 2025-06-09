2f38f9c955f56f291aae41a665ce386408b8c554
Kayla Man <moonyuet2008@hotmail.com>
2025-06-09 16:35:17 +0800

    publish the relevant instance data for the remote publish

--- client/ayon_maya/plugins/publish/collect_cache_farm.py
+++ client/ayon_maya/plugins/publish/collect_cache_farm.py
@@ -13,16 +13,6 @@ class CollectDataforCache(plugin.MayaInstancePlugin):
     label = "Collect Data for Cache"
 
     def process(self, instance):
-        # Should we implement this in somewhere else?
-        tmp_staging_dir = os.path.join(os.environ["AYON_WORKDIR"], "cache", "alembic")
-        os.makedirs(tmp_staging_dir, exist_ok=True)
-        # TODO: Support format other than alembic
-        filename = "{name}.abc".format(**instance.data)
-        expected_filepath = os.path.join(tmp_staging_dir, filename)
-        files = instance.data.setdefault("files", list())
-        files.append(expected_filepath)
-        expected_files = instance.data.setdefault("expectedFiles", list())
-        expected_files.append({"cache": files})
         instance.data.update({
             # used in MayaCacheSubmitDeadline in ayon-deadline
             "plugin": "MayaBatch",
