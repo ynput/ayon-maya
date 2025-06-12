d31a8d044c007b62a6ebc9f84b9a1f974e82cb46
Kayla Man <moonyuet2008@hotmail.com>
2025-06-12 17:01:15 +0800

    rename workfile_publish_on_farm to remote_publish_on_farm

--- client/ayon_maya/plugins/publish/collect_animation.py
+++ client/ayon_maya/plugins/publish/collect_animation.py
@@ -56,4 +56,4 @@ class CollectAnimationOutputGeometry(plugin.MayaInstancePlugin):
         instance.data["out_hierarchy"] = hierarchy
 
         if instance.data.get("farm"):
-            instance.data["families"].append("workfile_publish_on_farm")
+            instance.data["families"].append("remote_publish_on_farm")
