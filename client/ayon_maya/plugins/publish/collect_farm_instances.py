0bfb56ca7c608cb161ed7fc04ee57977dca90c42
Kayla Man <moonyuet2008@hotmail.com>
2025-06-12 19:47:11 +0800

    add generic collector for adding remote publish extractor for farm caching

---
+++ client/ayon_maya/plugins/publish/collect_farm_instances.py
@@ -0,0 +1,32 @@
+import pyblish.api
+from ayon_maya.api import plugin
+
+
+class CollectFarmInstances(plugin.MayaInstancePlugin):
+    """Collect Cache instances for publish, only works for headless mode
+
+    """
+
+    order = pyblish.api.CollectorOrder + 0.223
+    label = "Collect Farm Instances"
+    families = ["animation", "pointcache"]
+    targets = ["local"]
+
+    def process(self, instance):
+        if instance.data.get("farm"):
+            instance.data["families"].append("remote_publish_on_farm")
+
+
+class CollectRemoteCacheInstances(plugin.MayaInstancePlugin):
+    """Collect Cache instances for publish, only works for headless mode
+
+    """
+
+    order = pyblish.api.CollectorOrder + 0.223
+    label = "Collect Remote Cache Instances"
+    families = ["animation", "pointcache"]
+    targets = ["remote"]
+
+    def process(self, instance):
+        self.log.debug("Processing Cache Farm Instances.")
+        instance.data["farm"] = False
