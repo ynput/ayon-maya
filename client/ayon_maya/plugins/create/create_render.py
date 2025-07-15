62ab2bbdc4a299cf77b950dce81a974c36c3f5cd
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-15 23:25:13 +0200

    Fix super call after class name change

--- client/ayon_maya/plugins/create/create_render.py
+++ client/ayon_maya/plugins/create/create_render.py
@@ -54,9 +54,7 @@ class CreateRender(plugin.RenderlayerCreator):
         if self.render_settings.get("apply_render_settings"):
             lib_rendersettings.RenderSettings().set_default_renderer_settings()
 
-        super(CreateRenderlayer, self).create(product_name,
-                                              instance_data,
-                                              pre_create_data)
+        super().create(product_name, instance_data, pre_create_data)
 
     def get_instance_attr_defs(self):
         """Create instance settings."""
