2942986ffca844d01b63023f7f3930718b868dc2
Roy Nieterau <roy_nieterau@hotmail.com>
2025-07-23 00:09:49 +0200

    Add backwards compatibility

--- client/ayon_maya/plugins/publish/increment_current_file_deadline.py
+++ client/ayon_maya/plugins/publish/increment_current_file_deadline.py
@@ -2,7 +2,6 @@ import pyblish.api
 
 from ayon_core.lib import version_up
 from ayon_core.host import IWorkfileHost
-from ayon_core.host.interfaces import SaveWorkfileOptionalData
 from ayon_core.pipeline import registered_host
 from ayon_maya.api import plugin
 
@@ -21,15 +20,21 @@ class IncrementCurrentFileDeadline(plugin.MayaContextPlugin):
         new_filepath = version_up(current_filepath)
 
         host: IWorkfileHost = registered_host()
-        host.save_workfile_with_context(
-            filepath=new_filepath,
-            folder_entity=context.data["folderEntity"],
-            task_entity=context.data["taskEntity"],
-            description="Incremented by publishing.",
-            # Optimize the save by reducing needed queries for context
-            prepared_data=SaveWorkfileOptionalData(
-                project_entity=context.data["projectEntity"],
-                project_settings=context.data["project_settings"],
-                anatomy=context.data["anatomy"],
+        if hasattr(host, "save_workfile_with_context"):
+            from ayon_core.host.interfaces import SaveWorkfileOptionalData
+            host.save_workfile_with_context(
+                filepath=new_filepath,
+                folder_entity=context.data["folderEntity"],
+                task_entity=context.data["taskEntity"],
+                description="Incremented by publishing.",
+                # Optimize the save by reducing needed queries for context
+                prepared_data=SaveWorkfileOptionalData(
+                    project_entity=context.data["projectEntity"],
+                    project_settings=context.data["project_settings"],
+                    anatomy=context.data["anatomy"],
+                )
             )
-        )
+        else:
+            # Backwards compatibility before:
+            # https://github.com/ynput/ayon-core/pull/1275
+            host.save_workfile(new_filepath)
