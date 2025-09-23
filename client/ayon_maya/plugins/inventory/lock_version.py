f939da24e884f5c42620e69088688d3091b1230b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-09-23 13:40:30 +0200

    add lock/unlock version action

---
+++ client/ayon_maya/plugins/inventory/lock_version.py
@@ -0,0 +1,44 @@
+from ayon_core.pipeline import InventoryAction
+from ayon_maya.api.lib import imprint, get_container_members
+
+
+class LockVersions(InventoryAction):
+    label = "Lock versions"
+    icon = "lock"
+    color = "#ffffff"
+    order = -1
+
+    @staticmethod
+    def is_compatible(container):
+        return container.get("version_locked") is not True
+
+    def process(self, containers):
+        for container in containers:
+            if container.get("version_locked") is True:
+                continue
+            container["version_locked"] = True
+            members = get_container_members(container)
+            for member in members:
+                imprint(member, {"version_locked": True})
+        return True
+
+
+class UnlockVersions(InventoryAction):
+    label = "Unlock versions"
+    icon = "lock-open"
+    color = "#ffffff"
+    order = -1
+
+    @staticmethod
+    def is_compatible(container):
+        return container.get("version_locked") is True
+
+    def process(self, containers):
+        for container in containers:
+            if container.get("version_locked") is not True:
+                continue
+            container["version_locked"] = False
+            members = get_container_members(container)
+            for member in members:
+                imprint(member, {"version_locked": False})
+        return True
