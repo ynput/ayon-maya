16a09765c673571934dcc90c92494e4d90d4d22b
Kayla Man <moonyuet2008@hotmail.com>
2026-08-03 16:23:38 +0800

    Rename the title of the settings regarding to discussion

--- server/settings/templated_workfile_settings.py
+++ server/settings/templated_workfile_settings.py
@@ -21,15 +21,15 @@ class WorkfileBuildProfilesModel(BaseSettingsModel):
         title="Keep placeholders")
     execute_on_new_file: bool = SettingsField(
         False,
-        title="Always apply to empty scene"
+        title="Apply to New Scene"
     )
     execute_on_app_launch: bool = SettingsField(
         True,
-        title="Apply on application launch"
+        title="Apply on Maya launch"
     )
     create_first_version: bool = SettingsField(
         True,
-        title="Create first version"
+        title="Save first workfile version"
     )
 
 
