671e38d3bbb2fa2430e377c752991cc8c6f15be2
Kayla Man <moonyuet2008@hotmail.com>
2026-08-21 21:44:45 +0800

    add "mayaUsd.filterProperties" as families.

--- client/ayon_maya/plugins/create/create_maya_usd.py
+++ client/ayon_maya/plugins/create/create_maya_usd.py
@@ -259,4 +259,4 @@ class CreateMayaUsdModel(CreateMayaUsd):
         return attr_defs
 
     def get_publish_families(self):
-        return ["model", "usd"]
+        return ["model", "usd", "mayaUsd.filterProperties"]
