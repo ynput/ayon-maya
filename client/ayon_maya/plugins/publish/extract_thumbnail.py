f37a5349b662d47b33edb5bee019a8db39df259a
Kayla Man <moonyuet2008@hotmail.com>
2026-08-27 22:58:21 +0800

    add comment

--- client/ayon_maya/plugins/publish/extract_thumbnail.py
+++ client/ayon_maya/plugins/publish/extract_thumbnail.py
@@ -54,6 +54,10 @@ class ExtractThumbnail(plugin.MayaExtractorPlugin,
             start=1, end=1,
             capture_preset=capture_preset)
         if preset["viewport_options"].get("bluePencil"):
+            # Force thumbnail capture to JPG if blue pencil is enabled
+            # as png captures alpha channels which maps out only the
+            # blue pencil's annotation and it doesn't show the composited
+            # version of the thumbnail.
             self.log.debug(
                 "Blue Pencil is enabled; forcing thumbnail capture to JPG "
                 "so annotations are composited over the thumbnail image "
