544369837be9389f72e9482e93c853337faf26de
Roy Nieterau <roy_nieterau@hotmail.com>
2026-09-21 10:52:34 +0200

    Use single listConnections query

--- client/ayon_maya/plugins/publish/validate_glsl_material.py
+++ client/ayon_maya/plugins/publish/validate_glsl_material.py
@@ -35,15 +35,15 @@ class ValidateGLSLMaterial(plugin.MayaInstancePlugin,
 
     def get_material_from_shapes(self, instance):
         shapes = cmds.ls(instance, type="mesh", long=True)
-        shading_groups = []
-        for shape in shapes:
-            shading_grp = cmds.listConnections(shape,
-                                               destination=True,
-                                               type="shadingEngine")
-            for shading_group in shading_grp or []:
-                if shading_group not in shading_groups:
-                    shading_groups.append(shading_group)
-        return shading_groups
+        shading_groups = cmds.listConnections(
+            shapes,
+            destination=True,
+            source=False,
+            type="shadingEngine",
+        ) or []
+        # Note: listConnections will return duplicates if multiple shapes share
+        #  the same shading group so we remove them here
+        return list(set(shading_groups))
 
     def get_texture_shader_invalid(self, instance):
 
