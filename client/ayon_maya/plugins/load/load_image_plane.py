2745bc7f624ad5c61e3318ab54017dad94d143be
Roy Nieterau <roy_nieterau@hotmail.com>
2026-09-21 10:50:15 +0200

    Use tuples instead

--- client/ayon_maya/plugins/load/load_image_plane.py
+++ client/ayon_maya/plugins/load/load_image_plane.py
@@ -238,12 +238,12 @@ class ImagePlaneLoader(plugin.Loader):
         start_frame = folder_entity["attrib"]["frameStart"]
         end_frame = folder_entity["attrib"]["frameEnd"]
 
-        for attr, value in {
-            "frameOffset": 0,
-            "frameIn": start_frame,
-            "frameOut": end_frame,
-            "frameCache": end_frame
-        }.items():
+        for attr, value in (
+            ("frameOffset", 0),
+            ("frameIn", start_frame),
+            ("frameOut", end_frame),
+            ("frameCache", end_frame),
+        ):
             plug = "{}.{}".format(image_plane_shape, attr)
             cmds.setAttr(plug, value)
 
